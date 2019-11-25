515cedc382594617224b1537afae18e7a2524a27
Dong Zhixuan <1034231982@qq.com>
2019-11-25 15:38:23 +0800

    1740610603董智轩修复参数传递错误

--- Code/cde.java
+++ Code/cde.java
@@ -22,7 +22,7 @@ public void login(String ID,String password,Stage stage){
         }else if(ID.length()==6){
             System.out.println("账号为老师");
             controller=new Teacher_Controller();
-            int status=controller.LoginC(ID);
+            int status=controller.LoginC(ID,password);
             if(status==1){
                 if(remember_id.isSelected())
                     USERINFO.put(ID,password);
